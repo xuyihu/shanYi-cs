4bc85eb554b4be933ad1abd13604840c01ffb8cf
xuyihu <xuyihu@jtexpress.com>
2023-06-12 16:13:22 +0800

    init

---
+++ demo-java/src/test/java/com/cs/demojava/DemoJavaApplicationTests.java
@@ -0,0 +1,14 @@
+package com.cs.demojava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println(111);
+    }
+
+}
